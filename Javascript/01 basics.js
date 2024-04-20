a387c164af60c276ed675fd95ae9b73c23002739
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2024-04-20 19:21:27 +0530

    Create 01 basics.js

---
+++ Javascript/01 basics.js
@@ -0,0 +1,26 @@
+console.log("Hello");
+
+// Arithematic operation
+console.log(2 + 3);
+console.log(23 - 5);
+console.log(4 * 3);
+console.log(10 / 2);
+
+
+// Alert
+
+
+alert("This is a alert"); 
+
+
+// confirm 
+
+confirm("Do you want to proceed");
+
+
+// Prompt 
+
+prompt("What is your name", "Shiv")
+
+
+
